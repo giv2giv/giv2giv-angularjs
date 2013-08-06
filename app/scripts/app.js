b9b3ae253563b7fee18875abe9a8425abb3b4f4a
Ray Daly <raydaly@gmail.com>
2013-08-06 16:39:04 -0400

    initial commit - need to add status

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('angularjsApp', [])
+angular.module('giv2givApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angularjsApp')
+var giv2givApp=angular.module('giv2givApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
@@ -8,3 +8,40 @@ angular.module('angularjsApp')
       'Karma'
     ];
   });
+
+// This controller is logging into giv2givApp
+giv2givApp.controller('LoginCtrl', function ($scope, $http){
+	$scope.message='';
+	$scope.submitLogin = function(){
+		//send required fields of email and password
+		//alert('submit code goes here');
+
+// curl -X POST -H "Content-Type: application/json" -d 
+// '{"email":"kmiller@ltc.com","password":"welcome"}' 
+// http://www.giv2giv.org/api/sessions/create.json
+
+		var data={}; //put in form here
+		$http({method: 'POST', data: data, url: 'http://www.giv2giv.org/api/sessions/create.json'}).
+		  success(function(data, status, headers) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+		    $scope.message ='Success. You are logged in. ';
+		  }).
+		  error(function(data, status, headers) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		    $scope.message ='Opps. Something went wrong. '+status;
+		  });
+	};
+
+});
+
+giv2givApp.config(function($httpProvider){
+	// TODO this tell how to do CORS with Angular
+	// http://better-inter.net/enabling-cors-in-angular-js/
+	// need to put all this into a service or factory, I think
+	//ALSO look at: http://stackoverflow.com/questions/15411818/setting-up-cors-with-angular-js
+	//$httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  delete $httpProvider.defaults.headers.post['Content-type'];
+});
