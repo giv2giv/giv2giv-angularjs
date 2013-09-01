025049dfad1e6bb4d44667f55bf044b8d556fe55
ozanzal <oz@o3design.net>
2013-08-31 23:22:34 -0400

    Adding some  controller logic

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,14 +1,32 @@
 'use strict';
 
 angular.module('giv2givApp')
-  .controller('CreateCtrl', function ($scope, $location) {
+  .controller('CreateCtrl', function ( $scope, $location  ) {
   	$scope.endowment = {};
     $scope.endowment.name = "";
     $scope.endowment.description = "";
     $scope.endowment.charities = [];
-    $scope.removeCharity = function(){ alert() };
-    $scope.addCharity = function(){ alert() };
-    $scope.saveEndowment = function(){
+
+    var charities = [
+      {name: "Charity One", id: 1234},
+      {name: "Charity Two", id: 1235},
+      {name: "Charity Three", id: 1236}
+    ];
+
+    $scope.endowment.charities = charities;
+
+    $scope.removeCharity = function ( charityId ){ 
+      $scope.endowment.charities.splice ( charityId, 1 );
+    };
+
+    $scope.addCharity = function ( charity ) {
+      if ( charity != undefined ) {
+        $scope.endowment.charities.push ( charity );
+      };
+    };
+
+    $scope.saveEndowment = function (){
     	$location.path('endowments/12345');
     };
+
   });
